6f0acf91e00ebccd43e26585a9b246168204bb9c
Yogel9 <79474789+Yogel9@users.noreply.github.com>
2021-03-09 17:34:03 +0300

    Anton branch (#2)
    * Create HelloWorldController.cs
    
    * Part3 View
    
    Работа с view
    
    * Part 3
    
    Добавлена папка HelloWorld в Views вместе с index(https://localhost:44324/HelloWorld),Welcome(https://localhost:44324/HelloWorld/Welcome?name=Rick&numtimes=4)
    
    Co-authored-by: Yogel9 <Dovidenkov@users.noreply.github.com>

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -1,24 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Text.Encodings.Web;
 
-namespace MvcMovie.Controllers
+namespace HelloMVC.Controllers
 {
+
     public class HelloWorldController : Controller
     {
         // 
         // GET: /HelloWorld/
-
-        public string Index()
+        public IActionResult Index()//ВЫЗЫВАЕМ ИДЕКС ИЗ VIEWS ПАПКИ
+        {
+            return View();
+        }
+        /*public string Index() Part2
         {
             return "This is my default action...";
+        }*/
+
+        //https://localhost:44324/HelloWorld/Welcome?name=Rick&numtimes=4
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
         }
 
-        // 
-        // GET: /HelloWorld/Welcome/ 
-        // Requires using System.Text.Encodings.Web;
+       /* // Requires using System.Text.Encodings.Web; Part2
         public string Welcome(string name, int ID = 1)
         {
             return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+
         }
+        */
     }
 }
