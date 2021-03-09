d0742668a2860ad4f89a87e353cb8347b3b5acac
Timofey Cherkashin <cherkel@users.noreply.github.com>
2021-03-09 17:56:56 +0300

    Part4(not full)
    Добавление модели ASP.NET, класса, базы данных, контроллера

---
+++ Data/HelloMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HelloMVC.Models;
+
+namespace HelloMVC.Data
+{
+    public class HelloMVCContext : DbContext
+    {
+        public HelloMVCContext(DbContextOptions<HelloMVCContext> options)
+           : base(options)
+        {
+        }
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
