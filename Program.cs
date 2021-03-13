f29cafbcb7f83126853bd46ee1ee0c255834ef5b
Yogel9 <Dovidenkov@users.noreply.github.com>
2021-03-13 21:03:29 +0300

    Revert "Part 5 Работа с базой данных"
    This reverts commit b76eafca510a99dce144fcc6dada5d7d7f3f0958.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using HelloMVC.Data;
-using HelloMVC.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace HelloMVC
 {
@@ -12,25 +13,7 @@ namespace HelloMVC
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
-                try
-                {
-                    SeedData.Initialize(services);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
-                }
-            }
-
-            host.Run();
-
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- dbo.Movie1.data.sql
+++
@@ -1,5 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[Movie] ON
-INSERT INTO [dbo].[Movie] ([Id], [Title], [ReleaseDate], [Genre], [Price]) VALUES (1, N'Hot Go', N'2021-03-03 00:00:00', N'Action', CAST(23.00 AS Decimal(18, 2)))
-INSERT INTO [dbo].[Movie] ([Id], [Title], [ReleaseDate], [Genre], [Price]) VALUES (2, N'Hot Go2', N'2021-03-24 00:00:00', N'Super Action', CAST(46.00 AS Decimal(18, 2)))
-INSERT INTO [dbo].[Movie] ([Id], [Title], [ReleaseDate], [Genre], [Price]) VALUES (3, N'сАША 2', N'2021-03-28 00:00:00', N'Action', CAST(46.00 AS Decimal(18, 2)))
-SET IDENTITY_INSERT [dbo].[Movie] OFF
